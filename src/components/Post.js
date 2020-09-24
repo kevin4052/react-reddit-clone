e7e862ec6c14462d226d70ff18625a0fdaddbda5
Kevin Hernandez <kevin.hernandez001@gmail.com>
2020-09-24 08:37:34 -0400

    intial commit

---
+++ src/components/Post.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+export default class Post extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: 0
+        };
+    }
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
